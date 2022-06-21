73868fd902e21c5c196f73f24c4ee35f39f86292
nathfborges <nathi-borges@hotmail.com>
2022-06-21 15:44:26 -0300

    Fixing other bugs

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(`mongodb://localhost:27017/lecteur`, {useNewUrlParser: true});
+mongoose.connect(`mongodb://mongo:27017/lecteur`, {useNewUrlParser: true});
 
 module.exports = mongoose;
